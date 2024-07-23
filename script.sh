ac67f923f529cb57d4d1b48cc16715538fd08ada
eros.montin <eros.montin@gmail.com>
2024-07-23 14:39:05 -0400

    v0

--- script.sh
+++ script.sh
@@ -5,15 +5,50 @@
 
 
 PT=$(pwd)
-cd /dcm
+mkdir /tmp/dicom
+cp /dcm/* /tmp/dicom
+
+rm /tmp/dicom/*.nii
+rm /tmp/dicom/*.json
+rm /tmp/dicom/*.nii.gz
+cd /tmp/dicom
 dcm2niix .
 
 mkdir /tmp/NIFTI
-mv /dcm/*.nii /tmp/NIFTI/
-mv /dcm/*.json /tmp/NIFTI/
+mv /tmp/dicom/*.nii /tmp/NIFTI/
+mv /tmp/dicom/*.json /tmp/NIFTI/
 cd $PT
 # Call the Julia program with the arguments
-echo julia fittingT2Maps.jl /dcm/ /db/$1 /nifti/
 julia fittingT2Maps.jl /tmp/NIFTI/ /db/$1 /nifti/
 
 
+
+
+
+# #!/bin/bash
+
+
+
+# PT=$(pwd)
+# mkdir /tmp/dicom
+# # Create symbolic links for all files in /tmp/dicom to /dcm
+# for file in `ls -d /dcm/*`
+# do
+#     ln -s $file /tmp/dicom/
+# done
+
+# cd /tmp/dicom
+# donedcm2niix .
+
+# mkdir /tmp/NIFTI
+# mv /tmp/dicom/*.nii /tmp/NIFTI/
+# mv /tmp/dicom/*.json /tmp/NIFTI/
+# cd $PT
+# # Call the Julia program with the arguments
+
+# julia fittingT2Maps.jl /tmp/NIFTI/ /db/$1 /nifti/
+
+# # Remove the symbolic links
+
+# rm -rf /tmp/dicom
+# rm -rf /tmp/NIFTI
