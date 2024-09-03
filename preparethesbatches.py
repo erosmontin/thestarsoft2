f7f57afee50597780d9461271ac9180491d24dd1
Eros <eros.montin@gmail.com>
2024-09-03 19:19:12 +0200

    bp

--- preparethesbatches.py
+++ preparethesbatches.py
@@ -63,7 +63,7 @@ def prepare_and_submit_jobs(file_path, DB, APP, JOB_DIR,outdir):
 
         cmd = f'''
         singularity exec -B /gpfs/data/denizlab/Datasets/OAI_original/00m/{p["Folder"]}:/dcm -B {OUTDIR}:/nifti \
-                -B {DB}:/db -B {app}:/app \
+                -B {DB}:/db -B {APP}:/app \
                 docker://erosmontin/thestarsoft2:latest \
                 /bin/bash -c "echo \$PWD && cd /app && bash script.sh VA23_Knee_7ETL_10TE.mat"
         '''
