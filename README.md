ca5831f1f6800368132a425118f5e09b028de096
Eros <eros.montin@gmail.com>
2024-09-04 12:02:09 +0200

    bp singularity

--- README.md
+++ README.md
@@ -25,5 +25,15 @@ docker build -t t2julia .
 docker run -v /home/eros/Downloads/Example/Example/10424416/:/dcm -v /g/nifti:/nifti -v /home/eros/Downloads/Example/Example/db:/db erosmontin/thestarsoft2:latest VA23_Knee_7ETL_10TE.mat
 ```
 
+
+
+
+### singularity
+pull the image
+```
+ singularity pull --name ~/thestarsoft2.sif docker://erosmontin/thestarsoft2:singularity
+
+```
+
 [*Dr. Eros Montin, PhD*](http://biodimensional.com)
 **46&2 just ahead of me!**
--- script_sy.sh
+++ script_sy.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 PT=$(pwd)
-mkdir /tmp/dicom
+mkdir -p /tmp/dicom
 cp /dcm/* /tmp/dicom
-rm /tmp/dicom/*.nii
-rm /tmp/dicom/*.json
-rm /tmp/dicom/*.nii.gz
+rm -f /tmp/dicom/*.nii
+rm -f /tmp/dicom/*.json
+rm -f /tmp/dicom/*.nii.gz
 echo "Converting DICOM to NIFTI"
 cd /tmp/dicom
-dcm2niix .
-echo "Moving NIFTI files to /tmp/NIFTI"
-mkdir /tmp/NIFTI
-mv /tmp/dicom/*.nii /tmp/NIFTI/
-mv /tmp/dicom/*.json /tmp/NIFTI/
-echo "Running Julia script"
-cd $PT
-julia /app/fittingT2Maps.jl /tmp/NIFTI/ /app/db/VA23_Knee_7ETL_10TE /nifti/
-echo "Fixing geometry"
+# dcm2niix .
+# echo "Moving NIFTI files to /tmp/NIFTI"
+# mkdir /tmp/NIFTI
+# mv /tmp/dicom/*.nii /tmp/NIFTI/
+# mv /tmp/dicom/*.json /tmp/NIFTI/
+# echo "Running Julia script"
+# cd $PT
+# julia /app/fittingT2Maps.jl /tmp/NIFTI/ /app/db/VA23_Knee_7ETL_10TE.mat /nifti/
+# echo "Fixing geometry"
+# python fix_geometry.py /tmp/NIFTI/ /nifti/
+dcm2niix . && \
+echo "Moving NIFTI files to /tmp/NIFTI" && \
+mkdir -p /tmp/NIFTI && \
+mv /tmp/dicom/*.nii /tmp/NIFTI/ && \
+mv /tmp/dicom/*.json /tmp/NIFTI/ && \
+echo "Running Julia script" && \
+cd $PT && \
+julia /app/fittingT2Maps.jl /tmp/NIFTI/ /app/db/VA23_Knee_7ETL_10TE.mat /nifti/ && \
+echo "Fixing geometry" && \
 python fix_geometry.py /tmp/NIFTI/ /nifti/
