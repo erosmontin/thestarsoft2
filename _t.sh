d6719b07c46a50654a47551f1a164444128788b1
Eros <eros.montin@gmail.com>
2024-09-04 15:57:33 +0200

    python

--- _t.sh
+++ _t.sh
@@ -20,7 +20,7 @@ echo "Moving NIFTI files to /gpfs/data/denizlab/Users/montie01/aaa/nifti" && \
 mkdir -p /gpfs/data/denizlab/Users/montie01/aaa/nifti && \
 mv /gpfs/data/denizlab/Users/montie01/aaa/dicom/*.nii /gpfs/data/denizlab/Users/montie01/aaa/nifti/ && \
 mv /gpfs/data/denizlab/Users/montie01/aaa/dicom/*.json /gpfs/data/denizlab/Users/montie01/aaa/nifti/ && \
-echo "Running Julia script" && \
+echo "Running Julia script"
 
 julia  /gpfs/home/montie01/PROJECTS/T2/thestarsoft2/OAI_DataProcessing/fittingT2Maps.jl /gpfs/data/denizlab/Users/montie01/aaa/nifti/ /app/db/VA23_Knee_7ETL_10TE.mat /nifti/ && \
 python fix_geometry.py /gpfs/data/denizlab/Users/montie01/aaa/nifti/ /data/denizlab/Users/montie01//T2/OUTDIR/00m/9000296SAG_T2_MAP_RIGHT
