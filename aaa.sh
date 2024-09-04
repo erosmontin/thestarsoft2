84fbc5e9a72d7b7ce1ff1b5f3bcafc0cdb02817b
Eros <eros.montin@gmail.com>
2024-09-04 17:46:31 +0200

    bash

---
+++ aaa.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --job-name=aaa.sh
+#SBATCH --output=C:\Users\montie01\Desktop\GIT\thestarsoft2/log.out
+#SBATCH --partition=cpu_short
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=10G
+module add dcm2niix/20211006
+module add julia/1.9.4
+
+FAKE=/gpfs/data/denizlab/Users/montie01/T2/JOBS/9000296SAG_T2_MAP_RIGHT
+rm -rf $FAKE
+DICOM=/gpfs/data/denizlab/Datasets/OAI_original/00m/0.C.2/9000296/20040909/10693717
+OUTPUTDIR=/gpfs/data/denizlab/Users/montie01/T2/OUTDIR/00m/9000296SAG_T2_MAP_RIGHT
+APP=/gpfs/home/montie01/PROJECTS/T2/thestarsoft2/OAI_DataProcessing/fittingT2Maps.jl
+
+FAKE_DICOM=$FAKE/dicom
+FAKE_NIFTI=$FAKE/nifti
+_DB=/gpfs/home/montie01/PROJECTS/T2/thestarsoft2/db/VA23_Knee_7ETL_10TE.matt
+
+mkdir -p $FAKE_DICOM
+mkdir -p $FAKE_NIFTI
+
+echo "Copying DICOM files to $FAKE_DICOM"
+cp $DICOM/* $FAKE_DICOM/
+
+rm -f $FAKE_DICOM/*.nii
+rm -f $FAKE_DICOM/*.json
+rm -f $FAKE_DICOM/*.nii.gz
+
+echo "Converting DICOM to NIFTI"
+dcm2niix $FAKE_DICOM/ && \
+
+echo "Moving NIFTI files to $FAKE_NIFTI"
+mv $FAKE_DICOM/*.nii $FAKE_NIFTI/
+mv $FAKE_DICOM/*.json $FAKE_NIFTI/
+
+echo "Running Julia script"
+echo julia $APP $FAKE_NIFTI/ $_DB $OUTPUTDIR/
+
+julia $APP $FAKE_NIFTI/ $_DB $OUTPUTDIR/
+python fix_geometry.py $FAKE_NIFTI/ $OUTPUTDIR/
--- preparethesbatches2.py
+++ preparethesbatches2.py
@@ -4,26 +4,64 @@ import numpy as np
 import os
 import time
 import glob
-def makeSlurm(jobName, job,partition='cpu_short', time='cpu_long', nodes='1', ntasks='1', cpus='2', mem='10G',modules=[]):
-    FN=jobName+ '.sh'
+def makeSlurm(jobName, dicomdir,tmp,app,outputdir,db, partition='cpu_short', time='02:00:00', nodes='1', ntasks='1', cpus='2', mem='10G'):
+    FN = jobName + '.sh'
     slurm = open(FN, 'w')
+    # get path of FN
+    FN_dir = os.path.dirname(os.path.abspath(FN))
+    FN_name = os.path.basename(FN)
+
+
     slurm.write('#!/bin/bash\n')
-    # slurm.write('#SBATCH --job-name=' + jobName + '\n')
-    slurm.write('#SBATCH --output=' + jobName + '.out\n')
+    slurm.write('#SBATCH --job-name='+FN_name+'\n')
+    slurm.write('#SBATCH --output=' + FN_dir + '/log.out\n')
     slurm.write('#SBATCH --partition=' + partition + '\n')
     slurm.write('#SBATCH --time=' + time + '\n')
     slurm.write('#SBATCH --nodes=' + nodes + '\n')
     slurm.write('#SBATCH --ntasks=' + ntasks + '\n')
     slurm.write('#SBATCH --cpus-per-task=' + cpus + '\n')
     slurm.write('#SBATCH --mem=' + mem + '\n')
-    if len(modules) > 0:
-        for module in modules:
-            slurm.write(f'module load {module}\n')
-    slurm.write(f'{job}\n')
+
+    slurm.write('module add dcm2niix/20211006\n')
+    slurm.write('module add julia/1.9.4\n\n')
+    
+    slurm.write(f'FAKE={tmp}\n')
+    slurm.write('rm -rf $FAKE\n')
+    slurm.write(f'DICOM={dicomdir}\n')
+    slurm.write(f'OUTPUTDIR={outputdir}\n')
+    slurm.write(f'APP={app}\n\n')
+    
+    slurm.write('FAKE_DICOM=$FAKE/dicom\n')
+    slurm.write('FAKE_NIFTI=$FAKE/nifti\n')
+    slurm.write(f'_DB={db}\n\n')
+    
+    slurm.write('mkdir -p $FAKE_DICOM\n')
+    slurm.write('mkdir -p $FAKE_NIFTI\n\n')
+    
+    slurm.write('echo "Copying DICOM files to $FAKE_DICOM"\n')
+    slurm.write('cp $DICOM/* $FAKE_DICOM/\n\n')
+    
+    slurm.write('rm -f $FAKE_DICOM/*.nii\n')
+    slurm.write('rm -f $FAKE_DICOM/*.json\n')
+    slurm.write('rm -f $FAKE_DICOM/*.nii.gz\n\n')
+    
+    slurm.write('echo "Converting DICOM to NIFTI"\n')
+    slurm.write('dcm2niix $FAKE_DICOM/ && \\\n\n')
+    
+    slurm.write('echo "Moving NIFTI files to $FAKE_NIFTI"\n')
+    slurm.write('mv $FAKE_DICOM/*.nii $FAKE_NIFTI/\n')
+    slurm.write('mv $FAKE_DICOM/*.json $FAKE_NIFTI/\n\n')
+    
+    slurm.write('echo "Running Julia script"\n')
+    slurm.write('echo julia $APP $FAKE_NIFTI/ $_DB $OUTPUTDIR/\n\n')
+    
+    slurm.write('julia $APP $FAKE_NIFTI/ $_DB $OUTPUTDIR/\n')
+    slurm.write('python fix_geometry.py $FAKE_NIFTI/ $OUTPUTDIR/\n')
+    
     slurm.close()
     return FN
 
-def prepare_and_submit_jobs(file_path, DB, APP, JOB_DIR,outdir,tmpdir,sif):
+def prepare_and_submit_jobs(file_path, DB, APP, TMP,JOB_DIR,outdir):
     file_extension = os.path.splitext(file_path)[1]
     if file_extension == '.xlsx':
         df = pd.read_excel(file_path)
@@ -32,13 +70,7 @@ def prepare_and_submit_jobs(file_path, DB, APP, JOB_DIR,outdir,tmpdir,sif):
     else:
         raise ValueError("Unsupported file extension. Only .xlsx and .csv are supported.")
 
-
     # Ensure directories exist
-    os.makedirs(APP, exist_ok=True,mode=0o775)
-    os.makedirs(JOB_DIR, exist_ok=True,mode=0o775)
-    MODULES=[]
-    MODULES = ['singularity/3.9.8']
-    JOBLIST = []
     FN = len(df)
     
     for a in range(0, FN):
@@ -47,48 +79,41 @@ def prepare_and_submit_jobs(file_path, DB, APP, JOB_DIR,outdir,tmpdir,sif):
         SERIES = p['SeriesDescription']
         PID=p['ParticipantID']
         NAME=f'{PID}{SERIES}'
-        OUTDIR = f'{outdir}/00m/{PID}{SERIES}/'
+        OUTDIR = f'{outdir}/00m/{NAME}'
         os.makedirs(OUTDIR, exist_ok=True)
-        # os.makedirs(app, exist_ok=True)
-        tmp=tmpdir+'/'+NAME
-        os.makedirs(tmp, exist_ok=True,mode=0o777)
-               # Wait until the directory is created and accessible
-        while not (os.path.exists(tmp) and os.access(tmp, os.W_OK)):
-            time.sleep(1)
+        tmp=TMP+'/'+NAME
+        os.makedirs(tmp, exist_ok=True)
 
-        DCM=f'/gpfs/data/denizlab/Datasets/OAI_original/00m/{p["Folder"]}'
-        if len(glob.glob(f'{DCM}/*'))<90:
-            print(f'No files in {DCM}')
+        dicom=f'/gpfs/data/denizlab/Datasets/OAI_original/00m/{p["Folder"]}'
+        if len(glob.glob(f'{dicom}/*'))<90:
+            print(f'No files in {dicom}')
             continue
-        # cmd = f'''mkdir -p -m 0777 {tmp} && singularity exec -B /gpfs/data/denizlab/Datasets/OAI_original/00m/{p["Folder"]}:/dcm -B {OUTDIR}:/nifti  -B {tmp}:/tmp docker://erosmontin/thestarsoft2:singularity /bin/bash -c "cd /app && bash script_sy.sh"'''
-        cmd = f''' singularity exec -B {DCM}:/dcm -B {OUTDIR}:/nifti  -B {tmp}:/tmp -B {APP}:/app {sif} /bin/bash -c "cd /app && bash script_sy.sh && rm -rf /tmp/dicom && rm -rf /tmp/NIFTI"'''
-
         job = f'{JOB_DIR}/job_{PID}_{SERIES}'
         # out= f'{OUTDIR}/job_{p["ParticipantID"]}.out'
-        fn=makeSlurm(f'{job}', cmd, partition='cpu_short', modules=MODULES, time='02:00:00')
+        fn=makeSlurm(f'{job}', dicom,tmp,APP,OUTDIR,DB,partition='cpu_short', time='02:00:00', nodes='1', ntasks='1', cpus='2', mem='10G')
         JOBLIST.append(fn)
     
     return JOBLIST
 
 # Example usage
+
+
+
+
+
 file_path = 'debug.csv'
-DB = '/gpfs/home/montie01/PROJECTS/OAI/'
-APP = '/gpfs/home/montie01/PROJECTS/T2/thestarsoft2'
+DB = '/gpfs/home/montie01/PROJECTS/T2/thestarsoft2/db/VA23_Knee_7ETL_10TE.mat'
+APP='/gpfs/home/montie01/PROJECTS/T2/thestarsoft2/OAI_DataProcessing/fittingT2Maps.jl'
 JOB_DIR = '/gpfs/data/denizlab/Users/montie01/T2/JOBS'
 try:
     os.rmdir(JOB_DIR)
 except:
     pass
 
-OUTDIR = '/gpfs/data/denizlab/Users/montie01//T2/OUTDIR'
+OUTDIR = '/gpfs/data/denizlab/Users/montie01/T2/OUTDIR'
 TMP='/gpfs/data/denizlab/Users/montie01/T2/_TMP'
-SIF='/gpfs/home/montie01/PROJECTS/T2/sif/thestarsoft2.sif'
-
-
-#run
-# julia -e 'using Pkg; Pkg.add(["MAT", "NIfTI", "LsqFit", "Statistics", "Plots", "NaturalSort", "Glob"]); Pkg.precompile()'
-
-JOBLIST = prepare_and_submit_jobs(file_path, DB, APP, JOB_DIR,outdir=OUTDIR,tmpdir=TMP,sif=SIF)
+ 
+JOBLIST = prepare_and_submit_jobs(file_path, DB, APP, JOB_DIR,TMP,OUTDIR)
 
 for job in JOBLIST:
     os.system(f'sbatch {job}')
